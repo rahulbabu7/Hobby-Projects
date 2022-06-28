53e9b4e1ef920c20daef039f325f6f9d100964b5
Rahul Babu <60734994+rahulbabu7@users.noreply.github.com>
2022-06-29 00:58:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+let count=0;
+
+function increment(){
+    if(count<12)
+    {
+      count+=1;
+      let value=document.getElementById("count")
+   value.innerHTML=count;
+    }
+     else
+
+     {
+        alert("maximum number of passengers should be 12")
+     }
+}
+
+ /*function decrement(){
+   
+    count -=1;
+    let counter=document.getElementById("left")
+    counter.innerHTML=count;
+    console.log(count);
+
+}*/
+
+function save(){
+
+    var saveData=count+"-"  
+    count=0;
+    let saves=document.getElementById("entry")
+ //   console.log(saveData);
+     saves.innerHTML+=saveData;    
+}
